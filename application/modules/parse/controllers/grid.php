414d1d80dec70c8c56930ef37845fd873fe52b63
Pavel Machekhin <pavel.machekhin@gmail.com>
2015-09-17 18:51:10 +0300

    Generate grid controller for parse module

---
+++ application/modules/parse/controllers/grid.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * @author   dev
+ * @created  2015-09-17 10:57:04
+ */
+namespace Application;
+
+use Bluz;
+
+return
+/**
+ * @return \closure
+ */
+function () use ($view) {
+    /**
+     * @var \Bluz\Application $this
+     * @var \Bluz\View\View $view
+     */
+    $request = $this->getRequest();
+};
